107f15ca5be91a8cf58d23dd291fdfce3a61690b
Dean Gibson <dean@deangibson.me>
2018-02-16 09:38:53 +0200

    Modified Badge component to be perfectly round

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components'
 
 const Badge = styled.span`
-  position: absolute;
-  font-family: monospace;
+  align-items: center;
   background-color: var(--reactour-accent);
-  height: 1.875em;
-  line-height: 2;
-  padding-left: .8125em;
-  padding-right: .8125em;
-  font-size: 1em;
   border-radius: 1.625em;
-  color: white;
-  text-align: center;
   box-shadow: 0 .25em .5em rgba(0,0,0,.3);
-  top: -.8125em;
+  color: white;
+  display: flex;
+  font-family: monospace;
+  font-size: 1em;
+  height: 1.875em;
+  justify-content: center;
   left: -.8125em;
+  position: absolute;
+  text-align: center;
+  top: -.8125em;
+  width: 1.875em;
 `
 
 export default Badge
