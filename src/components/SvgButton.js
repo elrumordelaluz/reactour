09a35ccb765574446bd43ff3f9c3782f63bf1e3a
Simon Pascal Klein <klepas@klepas.org>
2018-08-03 12:02:48 +1000

    Clean up cursor not-allowed | pointer styles.
    Signed-off-by: Simon Pascal Klein <klepas@klepas.org>

--- src/components/SvgButton.js
+++ src/components/SvgButton.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 export default styled.button`
   display: block;
-  cursor: pointer;
   padding: 0;
   outline: 0;
   border: 0;
   background: none;
   font-size: 0;
-`
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+  `
