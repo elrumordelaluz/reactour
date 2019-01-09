4325ea4dfd7c806fe188884fa03f62be2dba06b2
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-01-09 16:08:47 +0100

    Proposal change using createPortal

---
+++ src/Portal.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { createPortal } from 'react-dom'
+
+class Tour extends Component {
+  constructor(props) {
+    super(props)
+    this.el = document.createElement('div')
+    this.el.setAttribute('id', '___reactour')
+  }
+
+  componentDidMount() {
+    document.body.appendChild(this.el)
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.el)
+  }
+
+  render() {
+    return createPortal(this.props.children, this.el)
+  }
+}
+
+export default Tour
--- src/Tour.js
+++ src/Tour.js
@@ -1,78 +1,609 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
-import ExecutionEnvironment from 'exenv'
-import TourPortal from './TourPortal'
-
-const SafeHTMLElement = ExecutionEnvironment.canUseDOM ? window.HTMLElement : {}
-
-function getParentElement(parentSelector) {
-  return parentSelector()
-}
+import cn from 'classnames'
+import scrollSmooth from 'scroll-smooth'
+import Scrollparent from 'scrollparent'
+import {
+  Arrow,
+  Close,
+  Guide,
+  Badge,
+  Controls,
+  Navigation,
+  Dot,
+  SvgMask,
+} from './components/index'
+import Portal from './Portal'
+import * as hx from './helpers'
 
 class Tour extends Component {
   static propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    portalClassName: PropTypes.string,
-    appElement: PropTypes.instanceOf(SafeHTMLElement),
-    onAfterOpen: PropTypes.func,
-    onRequestClose: PropTypes.func,
+    badgeContent: PropTypes.func,
+    highlightedMaskClassName: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
+    className: PropTypes.string,
     closeWithMask: PropTypes.bool,
-    parentSelector: PropTypes.func,
+    inViewThreshold: PropTypes.number,
+    isOpen: PropTypes.bool.isRequired,
+    lastStepNextButton: PropTypes.node,
+    maskClassName: PropTypes.string,
+    maskSpace: PropTypes.number,
+    nextButton: PropTypes.node,
+    onAfterOpen: PropTypes.func,
+    onBeforeClose: PropTypes.func,
+    onRequestClose: PropTypes.func,
+    prevButton: PropTypes.node,
+    scrollDuration: PropTypes.number,
+    scrollOffset: PropTypes.number,
+    showButtons: PropTypes.bool,
+    showCloseButton: PropTypes.bool,
+    showNavigation: PropTypes.bool,
+    showNavigationNumber: PropTypes.bool,
+    showNumber: PropTypes.bool,
+    startAt: PropTypes.number,
+    goToStep: PropTypes.number,
+    getCurrentStep: PropTypes.func,
+    nextStep: PropTypes.func,
+    prevStep: PropTypes.func,
+    steps: PropTypes.arrayOf(
+      PropTypes.shape({
+        selector: PropTypes.string,
+        content: PropTypes.oneOfType([
+          PropTypes.node,
+          PropTypes.element,
+          PropTypes.func,
+        ]).isRequired,
+        position: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'center']),
+        action: PropTypes.func,
+        style: PropTypes.object,
+        stepInteraction: PropTypes.bool,
+      })
+    ),
+    update: PropTypes.string,
+    updateDelay: PropTypes.number,
+    disableInteraction: PropTypes.bool,
+    disableDotsNavigation: PropTypes.bool,
+    disableKeyboardNavigation: PropTypes.bool,
+    rounded: PropTypes.number,
+    accentColor: PropTypes.string,
   }
 
   static defaultProps = {
-    isOpen: false,
-    portalClassName: 'reactour-portal',
+    showNavigation: true,
+    showNavigationNumber: true,
+    showButtons: true,
+    showCloseButton: true,
+    showNumber: true,
+    scrollDuration: 1,
+    maskSpace: 10,
+    updateDelay: 1,
+    disableInteraction: false,
+    rounded: 0,
+    accentColor: '#007aff',
     closeWithMask: true,
-    parentSelector() {
-      return document.body
-    },
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false,
+      current: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      width: 0,
+      height: 0,
+      w: 0,
+      h: 0,
+      inDOM: false,
+      observer: null,
+    }
+    this.helper = createRef()
+    this.helperElement = null
   }
 
   componentDidMount() {
-    this.node = document.createElement('div')
-    this.node.className = this.props.portalClassName
-    const parent = getParentElement(this.props.parentSelector)
-    parent.appendChild(this.node)
-    this.renderPortal(this.props)
+    const { isOpen, startAt } = this.props
+    if (isOpen) {
+      this.open(startAt)
+    }
   }
 
   componentWillReceiveProps(nextProps) {
-    const currentParent = getParentElement(this.props.parentSelector)
-    const newParent = getParentElement(nextProps.parentSelector)
+    const { isOpen, update, updateDelay } = this.props
 
-    if (newParent !== currentParent) {
-      currentParent.removeChild(this.node)
-      newParent.appendChild(this.node)
+    if (!isOpen && nextProps.isOpen) {
+      this.open(nextProps.startAt)
+    } else if (isOpen && !nextProps.isOpen) {
+      this.close()
     }
 
-    this.renderPortal(nextProps)
+    if (isOpen && update !== nextProps.update) {
+      if (nextProps.steps[this.state.current]) {
+        setTimeout(this.showStep, updateDelay)
+      } else {
+        this.props.onRequestClose()
+      }
+    }
+
+    if (
+      isOpen &&
+      nextProps.isOpen &&
+      this.state.current !== nextProps.goToStep
+    ) {
+      this.gotoStep(nextProps.goToStep)
+    }
   }
 
   componentWillUnmount() {
-    this.removePortal()
+    const { isOpen } = this.props
+    if (isOpen) {
+      this.close()
+    }
+    if (this.state.observer) {
+      this.state.observer.disconnect()
+    }
   }
 
-  renderPortal(props) {
-    if (props.isOpen) {
-      document.body.classList.add('reactour__body')
-    } else {
-      document.body.classList.remove('reactour__body')
+  open(startAt) {
+    const { onAfterOpen } = this.props
+    this.setState(
+      prevState => ({
+        isOpen: true,
+        current: startAt !== undefined ? startAt : prevState.current,
+      }),
+      () => {
+        this.showStep()
+        this.helperElement = this.helper.current
+        this.helper.current.focus()
+        if (onAfterOpen) {
+          onAfterOpen(this.helperElement)
+        }
+      }
+    )
+    // TODO: debounce it.
+    window.addEventListener('resize', this.showStep, false)
+    window.addEventListener('keydown', this.keyDownHandler, false)
+  }
+
+  showStep = () => {
+    const { steps } = this.props
+    const { current } = this.state
+    const step = steps[current]
+    const node = step.selector ? document.querySelector(step.selector) : null
+
+    const stepCallback = o => {
+      if (step.action && typeof step.action === 'function') {
+        step.action(o)
+      }
     }
 
-    this.portal = ReactDOM.createPortal(<TourPortal {...props} />, this.node)
+    if (step.observe) {
+      const target = document.querySelector(step.observe)
+      const config = { attributes: true, childList: true, characterData: true }
+      this.setState(
+        prevState => {
+          if (prevState.observer) {
+            setTimeout(() => {
+              prevState.observer.disconnect()
+            }, 0)
+          }
+          return {
+            observer: new MutationObserver(mutations => {
+              mutations.forEach(mutation => {
+                if (
+                  mutation.type === 'childList' &&
+                  mutation.addedNodes.length > 0
+                ) {
+                  const cb = () => stepCallback(mutation.addedNodes[0])
+                  setTimeout(
+                    () =>
+                      this.calculateNode(
+                        mutation.addedNodes[0],
+                        step.position,
+                        cb
+                      ),
+                    100
+                  )
+                } else if (
+                  mutation.type === 'childList' &&
+                  mutation.removedNodes.length > 0
+                ) {
+                  const cb = () => stepCallback(node)
+                  this.calculateNode(node, step.position, cb)
+                }
+              })
+            }),
+          }
+        },
+        () => this.state.observer.observe(target, config)
+      )
+    } else {
+      if (this.state.observer) {
+        this.state.observer.disconnect()
+        this.setState({
+          observer: null,
+        })
+      }
+    }
+
+    if (node) {
+      const cb = () => stepCallback(node)
+      this.calculateNode(node, step.position, cb)
+    } else {
+      this.setState(
+        setNodeState(null, this.helper.current, step.position),
+        stepCallback
+      )
+
+      step.selector &&
+        console.warn(
+          `Doesn't find a DOM node '${step.selector}'.
+                    Please check the 'steps' Tour prop Array at position: ${current +
+                      1}.`
+        )
+    }
   }
 
-  removePortal() {
-    ReactDOM.unmountComponentAtNode(this.node)
-    const parent = getParentElement(this.props.parentSelector)
-    parent.removeChild(this.node)
-    document.body.classList.remove('reactour__body')
+  calculateNode = (node, stepPosition, cb) => {
+    const { scrollDuration, inViewThreshold, scrollOffset } = this.props
+    const attrs = hx.getNodeRect(node)
+    const w = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth || 0
+    )
+    const h = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight || 0
+    )
+    if (!hx.inView({ ...attrs, w, h, threshold: inViewThreshold })) {
+      const parentScroll = Scrollparent(node)
+      scrollSmooth.to(node, {
+        context: hx.isBody(parentScroll) ? window : parentScroll,
+        duration: scrollDuration,
+        offset: scrollOffset || -(h / 2),
+        callback: nd => {
+          this.setState(setNodeState(nd, this.helper.current, stepPosition), cb)
+        },
+      })
+    } else {
+      this.setState(setNodeState(node, this.helper.current, stepPosition), cb)
+    }
+  }
+
+  close() {
+    this.setState(prevState => {
+      if (prevState.observer) {
+        prevState.observer.disconnect()
+      }
+      return {
+        isOpen: false,
+        observer: null,
+      }
+    }, this.onBeforeClose)
+    window.removeEventListener('resize', this.showStep)
+    window.removeEventListener('keydown', this.keyDownHandler)
+  }
+
+  onBeforeClose() {
+    const { onBeforeClose } = this.props
+    if (onBeforeClose) {
+      onBeforeClose(this.helperElement)
+    }
+  }
+
+  maskClickHandler = e => {
+    const { closeWithMask, onRequestClose } = this.props
+    if (
+      closeWithMask &&
+      !e.target.classList.contains(CN.mask.disableInteraction)
+    ) {
+      onRequestClose(e)
+    }
+  }
+
+  nextStep = () => {
+    const { steps, getCurrentStep } = this.props
+    this.setState(prevState => {
+      const nextStep =
+        prevState.current < steps.length - 1
+          ? prevState.current + 1
+          : prevState.current
+
+      if (typeof getCurrentStep === 'function') {
+        getCurrentStep(nextStep)
+      }
+
+      return {
+        current: nextStep,
+      }
+    }, this.showStep)
+  }
+
+  prevStep = () => {
+    const { getCurrentStep } = this.props
+    this.setState(prevState => {
+      const nextStep =
+        prevState.current > 0 ? prevState.current - 1 : prevState.current
+
+      if (typeof getCurrentStep === 'function') {
+        getCurrentStep(nextStep)
+      }
+
+      return {
+        current: nextStep,
+      }
+    }, this.showStep)
+  }
+
+  gotoStep = n => {
+    const { steps, getCurrentStep } = this.props
+    this.setState(prevState => {
+      const nextStep = steps[n] ? n : prevState.current
+
+      if (typeof getCurrentStep === 'function') {
+        getCurrentStep(nextStep)
+      }
+
+      return {
+        current: nextStep,
+      }
+    }, this.showStep)
+  }
+
+  keyDownHandler = e => {
+    const {
+      onRequestClose,
+      nextStep,
+      prevStep,
+      disableKeyboardNavigation,
+    } = this.props
+    e.stopPropagation()
+
+    if (disableKeyboardNavigation) {
+      return
+    }
+
+    if (e.keyCode === 27) {
+      // esc
+      e.preventDefault()
+      onRequestClose()
+    }
+    if (e.keyCode === 39) {
+      // right
+      e.preventDefault()
+      typeof nextStep === 'function' ? nextStep() : this.nextStep()
+    }
+    if (e.keyCode === 37) {
+      // left
+      e.preventDefault()
+      typeof prevStep === 'function' ? prevStep() : this.prevStep()
+    }
   }
 
   render() {
-    return this.portal || null
+    const {
+      className,
+      steps,
+      maskClassName,
+      showButtons,
+      showCloseButton,
+      showNavigation,
+      showNavigationNumber,
+      showNumber,
+      onRequestClose,
+      maskSpace,
+      lastStepNextButton,
+      nextButton,
+      prevButton,
+      badgeContent,
+      highlightedMaskClassName,
+      disableInteraction,
+      disableDotsNavigation,
+      nextStep,
+      prevStep,
+      rounded,
+      accentColor,
+    } = this.props
+
+    const {
+      isOpen,
+      current,
+      inDOM,
+      top: targetTop,
+      right: targetRight,
+      bottom: targetBottom,
+      left: targetLeft,
+      width: targetWidth,
+      height: targetHeight,
+      w: windowWidth,
+      h: windowHeight,
+      helperWidth,
+      helperHeight,
+      helperPosition,
+    } = this.state
+
+    if (isOpen) {
+      return (
+        <Portal>
+          <div
+            ref={c => (this.mask = c)}
+            onClick={this.maskClickHandler}
+            className={cn(CN.mask.base, {
+              [CN.mask.isOpen]: isOpen,
+            })}
+          >
+            <SvgMask
+              windowWidth={windowWidth}
+              windowHeight={windowHeight}
+              targetWidth={targetWidth}
+              targetHeight={targetHeight}
+              targetTop={targetTop}
+              targetLeft={targetLeft}
+              padding={maskSpace}
+              rounded={rounded}
+              className={maskClassName}
+              disableInteraction={
+                disableInteraction && steps[current].stepInteraction
+                  ? !steps[current].stepInteraction
+                  : disableInteraction
+              }
+              disableInteractionClassName={`${
+                CN.mask.disableInteraction
+              } ${highlightedMaskClassName}`}
+            />
+          </div>
+          <Guide
+            ref={this.helper}
+            targetHeight={targetHeight}
+            targetWidth={targetWidth}
+            targetTop={targetTop}
+            targetRight={targetRight}
+            targetBottom={targetBottom}
+            targetLeft={targetLeft}
+            windowWidth={windowWidth}
+            windowHeight={windowHeight}
+            helperWidth={helperWidth}
+            helperHeight={helperHeight}
+            helperPosition={helperPosition}
+            padding={maskSpace}
+            tabIndex={-1}
+            current={current}
+            style={steps[current].style ? steps[current].style : {}}
+            rounded={rounded}
+            className={cn(CN.helper.base, className, {
+              [CN.helper.isOpen]: isOpen,
+            })}
+            accentColor={accentColor}
+          >
+            {steps[current] &&
+              (typeof steps[current].content === 'function'
+                ? steps[current].content({
+                    close: onRequestClose,
+                    goTo: this.gotoStep,
+                    inDOM,
+                    step: current + 1,
+                  })
+                : steps[current].content)}
+            {showNumber && (
+              <Badge data-tour-elem="badge">
+                {typeof badgeContent === 'function'
+                  ? badgeContent(current + 1, steps.length)
+                  : current + 1}
+              </Badge>
+            )}
+            {(showButtons || showNavigation) && (
+              <Controls data-tour-elem="controls">
+                {showButtons && (
+                  <Arrow
+                    onClick={
+                      typeof prevStep === 'function' ? prevStep : this.prevStep
+                    }
+                    disabled={current === 0}
+                    label={prevButton ? prevButton : null}
+                  />
+                )}
+
+                {showNavigation && (
+                  <Navigation data-tour-elem="navigation">
+                    {steps.map((s, i) => (
+                      <Dot
+                        key={`${s.selector ? s.selector : 'undef'}_${i}`}
+                        onClick={() => this.gotoStep(i)}
+                        current={current}
+                        index={i}
+                        disabled={current === i || disableDotsNavigation}
+                        showNumber={showNavigationNumber}
+                        data-tour-elem="dot"
+                      />
+                    ))}
+                  </Navigation>
+                )}
+
+                {showButtons && (
+                  <Arrow
+                    onClick={
+                      current === steps.length - 1
+                        ? lastStepNextButton
+                          ? onRequestClose
+                          : () => {}
+                        : typeof nextStep === 'function'
+                        ? nextStep
+                        : this.nextStep
+                    }
+                    disabled={
+                      !lastStepNextButton && current === steps.length - 1
+                    }
+                    inverted
+                    label={
+                      lastStepNextButton && current === steps.length - 1
+                        ? lastStepNextButton
+                        : nextButton
+                        ? nextButton
+                        : null
+                    }
+                  />
+                )}
+              </Controls>
+            )}
+
+            {showCloseButton ? <Close onClick={onRequestClose} /> : null}
+          </Guide>
+          {this.props.children}
+        </Portal>
+      )
+    }
+
+    return null
+  }
+}
+
+const CN = {
+  mask: {
+    base: 'reactour__mask',
+    isOpen: 'reactour__mask--is-open',
+    disableInteraction: 'reactour__mask--disable-interaction',
+  },
+  helper: {
+    base: 'reactour__helper',
+    isOpen: 'reactour__helper--is-open',
+  },
+}
+
+const setNodeState = (node, helper, position) => {
+  const w = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  )
+  const h = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  )
+  const { width: helperWidth, height: helperHeight } = hx.getNodeRect(helper)
+  const attrs = node
+    ? hx.getNodeRect(node)
+    : {
+        top: h + 10,
+        right: w / 2 + 9,
+        bottom: h / 2 + 9,
+        left: w / 2 - helperWidth / 2,
+        width: 0,
+        height: 0,
+        w,
+        h,
+        helperPosition: 'center',
+      }
+  return function update() {
+    return {
+      w,
+      h,
+      helperWidth,
+      helperHeight,
+      helperPosition: position,
+      ...attrs,
+      inDOM: node ? true : false,
+    }
   }
 }
 
