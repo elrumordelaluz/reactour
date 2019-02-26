71e3922b944c2f990e130c0094fe3af33cf9d1d3
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-02-26 17:43:57 +0100

    Move propTypes

--- src/Tour.js
+++ src/Tour.js
@@ -18,75 +18,6 @@ import Portal from './Portal'
 import * as hx from './helpers'
 
 class Tour extends Component {
-  static propTypes = {
-    badgeContent: PropTypes.func,
-    highlightedMaskClassName: PropTypes.string,
-    children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
-    className: PropTypes.string,
-    closeWithMask: PropTypes.bool,
-    inViewThreshold: PropTypes.number,
-    isOpen: PropTypes.bool.isRequired,
-    lastStepNextButton: PropTypes.node,
-    maskClassName: PropTypes.string,
-    maskSpace: PropTypes.number,
-    nextButton: PropTypes.node,
-    onAfterOpen: PropTypes.func,
-    onBeforeClose: PropTypes.func,
-    onRequestClose: PropTypes.func,
-    prevButton: PropTypes.node,
-    scrollDuration: PropTypes.number,
-    scrollOffset: PropTypes.number,
-    showButtons: PropTypes.bool,
-    showCloseButton: PropTypes.bool,
-    showNavigation: PropTypes.bool,
-    showNavigationNumber: PropTypes.bool,
-    showNumber: PropTypes.bool,
-    startAt: PropTypes.number,
-    goToStep: PropTypes.number,
-    getCurrentStep: PropTypes.func,
-    nextStep: PropTypes.func,
-    prevStep: PropTypes.func,
-    steps: PropTypes.arrayOf(
-      PropTypes.shape({
-        selector: PropTypes.string,
-        content: PropTypes.oneOfType([
-          PropTypes.node,
-          PropTypes.element,
-          PropTypes.func,
-        ]).isRequired,
-        position: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'center']),
-        action: PropTypes.func,
-        style: PropTypes.object,
-        stepInteraction: PropTypes.bool,
-      })
-    ),
-    update: PropTypes.string,
-    updateDelay: PropTypes.number,
-    disableInteraction: PropTypes.bool,
-    disableDotsNavigation: PropTypes.bool,
-    disableKeyboardNavigation: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.oneOf(['esc', 'right', 'left'])),
-      PropTypes.bool,
-    ]),
-    rounded: PropTypes.number,
-    accentColor: PropTypes.string,
-  }
-
-  static defaultProps = {
-    showNavigation: true,
-    showNavigationNumber: true,
-    showButtons: true,
-    showCloseButton: true,
-    showNumber: true,
-    scrollDuration: 1,
-    maskSpace: 10,
-    updateDelay: 1,
-    disableInteraction: false,
-    rounded: 0,
-    accentColor: '#007aff',
-    closeWithMask: true,
-  }
-
   constructor() {
     super()
     this.state = {
@@ -244,9 +175,9 @@ class Tour extends Component {
 
       step.selector &&
         console.warn(
-          `Doesn't find a DOM node '${step.selector}'.
-                    Please check the 'steps' Tour prop Array at position: ${current +
-                      1}.`
+          `Doesn't find a DOM node '${
+            step.selector
+          }'. Please check the 'steps' Tour prop Array at position ${current}.`
         )
     }
   }
@@ -655,6 +586,75 @@ const setNodeState = (node, helper, position) => {
       inDOM: node ? true : false,
     }
   }
+}
+
+Tour.propTypes = {
+  badgeContent: PropTypes.func,
+  highlightedMaskClassName: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
+  className: PropTypes.string,
+  closeWithMask: PropTypes.bool,
+  inViewThreshold: PropTypes.number,
+  isOpen: PropTypes.bool.isRequired,
+  lastStepNextButton: PropTypes.node,
+  maskClassName: PropTypes.string,
+  maskSpace: PropTypes.number,
+  nextButton: PropTypes.node,
+  onAfterOpen: PropTypes.func,
+  onBeforeClose: PropTypes.func,
+  onRequestClose: PropTypes.func,
+  prevButton: PropTypes.node,
+  scrollDuration: PropTypes.number,
+  scrollOffset: PropTypes.number,
+  showButtons: PropTypes.bool,
+  showCloseButton: PropTypes.bool,
+  showNavigation: PropTypes.bool,
+  showNavigationNumber: PropTypes.bool,
+  showNumber: PropTypes.bool,
+  startAt: PropTypes.number,
+  goToStep: PropTypes.number,
+  getCurrentStep: PropTypes.func,
+  nextStep: PropTypes.func,
+  prevStep: PropTypes.func,
+  steps: PropTypes.arrayOf(
+    PropTypes.shape({
+      selector: PropTypes.string,
+      content: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.element,
+        PropTypes.func,
+      ]).isRequired,
+      position: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'center']),
+      action: PropTypes.func,
+      style: PropTypes.object,
+      stepInteraction: PropTypes.bool,
+    })
+  ),
+  update: PropTypes.string,
+  updateDelay: PropTypes.number,
+  disableInteraction: PropTypes.bool,
+  disableDotsNavigation: PropTypes.bool,
+  disableKeyboardNavigation: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOf(['esc', 'right', 'left'])),
+    PropTypes.bool,
+  ]),
+  rounded: PropTypes.number,
+  accentColor: PropTypes.string,
+}
+
+Tour.defaultProps = {
+  showNavigation: true,
+  showNavigationNumber: true,
+  showButtons: true,
+  showCloseButton: true,
+  showNumber: true,
+  scrollDuration: 1,
+  maskSpace: 10,
+  updateDelay: 1,
+  disableInteraction: false,
+  rounded: 0,
+  accentColor: '#007aff',
+  closeWithMask: true,
 }
 
 export default Tour
