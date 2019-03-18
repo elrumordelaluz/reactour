b0c2ac918fca2927aa18150841f709b07a7cdede
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-03-18 22:53:12 +0100

    More hooks + order

---
+++ src/classNames.js
@@ -0,0 +1,15 @@
+export default {
+  mask: {
+    base: 'reactour__mask',
+    isOpen: 'reactour__mask--is-open',
+    disableInteraction: 'reactour__mask--disable-interaction',
+  },
+  helper: {
+    base: 'reactour__helper',
+    isOpen: 'reactour__helper--is-open',
+  },
+  dot: {
+    base: 'reactour__dot',
+    active: 'reactour__dot--is-active',
+  },
+}
