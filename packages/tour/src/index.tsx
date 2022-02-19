d74d6eae048e2238873a5c7a9ea35186d3cec3ad
Maks Hayduk <maksym.h@nerdysoft.com>
2022-02-19 13:05:06 +0200

    Provide exporting of prop types in @reactour/tour.

--- packages/tour/src/index.tsx
+++ packages/tour/src/index.tsx
@@ -3,5 +3,5 @@ import TourContext, { TourProvider, useTour } from './Context'
 export { default as withTour } from './withTour'
 export default Tour
 export { Tour, TourContext, TourProvider, useTour }
-export { StepType, Position } from './types'
+export { StepType, Position, ProviderProps, TourProps } from './types'
 export { components } from './components'
