deeba050bf067def7592d90780516594c547c788
Lionel <elrumordelaluz@hotmail.com>
2022-08-25 12:59:32 +0200

    Update web demo

---
+++ apps/web/components/Row.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export default styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
---
+++ apps/web/components/Section.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+type SectionProps = {
+  center?: boolean
+  align?: string
+}
+
+export default styled.section<SectionProps>`
+  min-height: 100vh;
+  max-width: 1024px;
+  margin: auto;
+  padding: 1em;
+  text-align: ${props =>
+    props.center ? 'center' : props.align ? props.align : 'left'};
+`
