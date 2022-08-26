3fa67ab21f8724fe7682d5f237e0ea69ab417793
Lionel <elrumordelaluz@hotmail.com>
2022-08-26 12:21:45 +0200

    Update next.config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,17 @@
-// const withTM = require('next-transpile-modules')([
-//   '@reactour/utils',
-//   '@reactour/mask',
-//   '@reactour/popover',
-//   '@reactour/tour',
-// ])
+const withTM = require('next-transpile-modules')([
+  '@reactour/utils',
+  // '@reactour/mask',
+  // '@reactour/popover',
+  // '@reactour/tour',
+])
 
-// module.exports = withTM({
-//   reactStrictMode: true,
-//   images: {
-//     domains: ['cdn.dribbble.com'],
-//   },
-// })
-
-module.exports = {
+module.exports = withTM({
   reactStrictMode: true,
-}
+  images: {
+    domains: ['cdn.dribbble.com'],
+  },
+})
+
+// module.exports = {
+//   reactStrictMode: true,
+// }
