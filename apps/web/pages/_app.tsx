3df6954d5e20654face81b52e1fa2571527fffde
Lionel <elrumordelaluz@hotmail.com>
2022-08-28 19:48:52 +0200

    Add styles manually as a temp workaround

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import '../styles.css'
-import '@reactour/popover/dist/index.css'
+// import '@reactour/popover/dist/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
