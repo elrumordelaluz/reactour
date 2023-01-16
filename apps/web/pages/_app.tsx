f1422739f8c3d3ade26dc97522b1dc0861f85948
Lionel <elrumordelaluz@hotmail.com>
2023-01-16 14:45:44 +0100

    Add @vercel/analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
+import { Analytics } from '@vercel/analytics/react'
 import '../styles.css'
 // import '@reactour/popover/dist/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+      <Analytics />
+    </>
   )
 }
