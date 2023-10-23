bc7bdf059c8a58db35c2a68c7f15b11a2a60daa9
Lionel <elrumordelaluz@hotmail.com>
2023-10-23 11:53:01 +0200

    Add GA into demo

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,12 +1,25 @@
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import { NextUIProvider } from '@nextui-org/react'
 import '../styles.css'
 // import '@reactour/popover/dist/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RVNNN272SS" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-RVNNN272SS');
+        `}
+      </Script>
+    </>
   )
 }
