1e2f544251ea8f78b3583e79a9ef7195dcf8a3d7
Lionel <elrumordelaluz@hotmail.com>
2023-11-08 09:12:27 +0100

    Fix issue with next.config for static export

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,33 +1,43 @@
 import nextra from 'nextra'
+import { PHASE_EXPORT } from 'next/constants.js'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   latex: true,
   flexsearch: {
-    codeblocks: false
+    codeblocks: false,
   },
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
 })
 
-export default withNextra({
-  reactStrictMode: true,
-  eslint: {
-    // ESLint behaves weirdly in this monorepo.
-    ignoreDuringBuilds: true
-  },
-  webpack(config) {
-    const allowedSvgRegex = /components\/icons\/.+\.svg$/
+export default function (phase) {
+  return withNextra({
+    ...(phase === PHASE_EXPORT
+      ? {
+          images: {
+            unoptimized: true,
+          },
+        }
+      : {}),
+    reactStrictMode: true,
+    eslint: {
+      // ESLint behaves weirdly in this monorepo.
+      ignoreDuringBuilds: true,
+    },
+    webpack(config) {
+      const allowedSvgRegex = /components\/icons\/.+\.svg$/
 
-    const fileLoaderRule = config.module.rules.find(rule =>
-      rule.test?.test?.('.svg')
-    )
-    fileLoaderRule.exclude = allowedSvgRegex
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.('.svg')
+      )
+      fileLoaderRule.exclude = allowedSvgRegex
 
-    config.module.rules.push({
-      test: allowedSvgRegex,
-      use: ['@svgr/webpack']
-    })
-    return config
-  }
-})
+      config.module.rules.push({
+        test: allowedSvgRegex,
+        use: ['@svgr/webpack'],
+      })
+      return config
+    },
+  })
+}
