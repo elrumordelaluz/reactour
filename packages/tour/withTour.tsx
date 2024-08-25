bfccb102bc1e36a6a02148a843fcc8a56ba7968d
程同学 <zenos.chen@foxmail.com>
2024-08-25 22:33:52 +0800

    Fix ts error about withTour

--- packages/tour/withTour.tsx
+++ packages/tour/withTour.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useTour } from './Context'
+import { TourProps } from '.'
 
 export default function withTour<P>(WrappedComponent: React.ComponentType<P>) {
-  const ComponentWithTour = (props: P) => {
+  const ComponentWithTour = (props: Exclude<P, keyof TourProps>) => {
     const tourProps = useTour()
     return <WrappedComponent {...props} {...tourProps} />
   }
