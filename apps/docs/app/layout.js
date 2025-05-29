c98ebecd9dff988319e6a78e31f1d6ceb978661a
Lionel <elrumordelaluz@hotmail.com>
2025-05-29 09:02:03 +0200

    Add missing tailwind support.
    closes #672 #666

--- apps/docs/app/layout.js
+++ apps/docs/app/layout.js
@@ -1,7 +1,7 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
 import Script from 'next/script'
+import './globals.css'
 
 export const metadata = {
   title: {
